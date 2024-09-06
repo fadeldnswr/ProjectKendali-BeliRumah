8c6b6fd241a91afb1e2fa1da1715ace71c4da144
Raffly Putera Wibowo <69633914+FrianVex@users.noreply.github.com>
2024-09-06 17:44:04 +0700

    Added Reset Password
    Skibidih

--- Account.py
+++ Account.py
@@ -1,47 +1,126 @@
-
+import json
+import numpy as np
 
 class Account():
 
     def __init__(self) -> None:
         pass
 
+    def read_credentials(self):
+        with open("accounts_credentials.json", "r") as file:
+            accounts_credentials = json.load(file)
+            file.seek(0)
+
+            for _ in accounts_credentials:
+                if accounts_credentials["unique_id"] == self.id_number:
+                    return accounts_credentials["username"]
+            
+
+    # Create Account Functions
     def create_account_username_password(self, parse_username, parse_password, parse_repassword):
 
-        username = parse_username
-        password = parse_password
+        self.id_number = np.random.randint(1000, 10000)
         retype_password = parse_repassword
 
-        if password == retype_password:
-            with open("accounts.txt", "w") as file:
-                file.write(username, password)
-            return True
+        account_credentials = {
+            "username": parse_username,
+            "password": parse_password,
+            "unique_id": self.id_number
+        }
 
+        if account_credentials["password"] == retype_password:
+            with open("accounts_credentials.json", "a") as file:
+                json.dump(account_credentials, file)
+            return True
+        
         else:
             return False
-        
-    def create_account_data(self):
-        full_name = input("Enter Full Name: ")
-        job = input("Masukkan Pekerjaan: ")
-        salary = input("Masukkan Gaji: ")
-        job_address = input("Masukkan Alamat Pekerjaan: ")
-        tanggungan = input("Masukkan Jumlah Tanggungan: ")
-        beban_cicilan = input("Masukkan Beban Cicilan: ")
-        tabungan = input("Masukkan Tabungan: ")
 
-        with open("accounts.txt", "w") as file:
-            file.write(full_name, job, salary, job_address, tanggungan, beban_cicilan, tabungan)
+    # Create Account Data Functions    
+    def create_account_data(self, parse_nama, parse_pekerjaan, parse_email, parse_gaji, parse_tempat_kerja, parse_tanggungan, parse_cicilan, parse_tabungan):
         
-        is_saved = 1
-        return is_saved
+        account_data = {
+            "unique_id": self.id_number, # Should Identify the Account of an user ideally yeah?
+            "full_name": parse_nama,
+            "email": parse_email,
+            "job": parse_pekerjaan,
+            "salary": parse_gaji,
+            "job_address": parse_tempat_kerja,
+            "tanggungan": parse_tanggungan,
+            "beban_cicilan": parse_cicilan,
+            "tabungan": parse_tabungan
+        }
+
+        with open("accounts_data.json", "a") as file:
+            json.dump(account_data, file)
+        return True
     
+    # Login Functions
     def user_login(self, parsed_username, parsed_password):
+        
         username = parsed_username
         password = parsed_password
-        with open("accounts.txt", "r") as file:
-            for line in file:
-                if username in line and password in line:
-                    return True
-                else:
-                    return False
+        with open("accounts_credentials.json", "r") as file:
+            credentials = json.load(file)
+            if credentials["username"] == username and credentials["password"] == password:
+                self.id_number = credentials["unique_id"]
+                return True
+            else:
+                return False
             
+    # Changing User Data Functions
+    def change_user_data(self, parse_nama, parse_pekerjaan, parse_email, parse_gaji, parse_tempat_kerja, parse_tanggungan, parse_cicilan, parse_tabungan):
         
+        account_data = {
+            "unique_id": self.id_number, # Should Identify the Account of an user ideally yeah?
+            "full_name": parse_nama,
+            "email": parse_email,
+            "job": parse_pekerjaan,
+            "salary": parse_gaji,
+            "job_address": parse_tempat_kerja,
+            "tanggungan": parse_tanggungan,
+            "beban_cicilan": parse_cicilan,
+            "tabungan": parse_tabungan
+        }
+
+        with open("accounts_data.json", "r+") as file:
+            accounts_data = json.load(file)
+            file.seek(0)
+            
+            for account in accounts_data:
+                if account["unique_id"] == self.id_number:
+                    account.update(account_data)
+            
+            file.truncate()
+            json.dump(accounts_data, file)
+
+        return True
+        
+    # Ressetting User Password Functions
+    def search_user_email(self, parse_email):
+        
+        with open("accounts_data.json", "r") as file:
+            data = json.load(file)
+            file.seek(0)
+            
+            for _ in data:
+                if data["email"] == parse_email:
+                    return True
+            else:
+                return False
+            
+    def reset_user_password(self, parse_password, parse_repassword):
+        
+        if parse_password == parse_repassword:
+            with open("accounts_credentials.json", "r+") as file:
+                credentials = json.load(file)
+                file.seek(0)
+            
+                for account in credentials:
+                    account["password"] = parse_password
+            
+                file.truncate()
+                json.dump(account, file)
+            return True
+        else:
+            return False
--- MainWindow.py
+++ MainWindow.py
@@ -5,6 +5,7 @@ import AppIcons_rc
 
 # Needed Libraries
 import time
+import json
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import *  # type: ignore
 from PySide2.QtGui import *  # type: ignore
@@ -24,12 +25,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.is_login = False
         self.is_match = False
         self.is_this_button_clicked = False
+        self.is_on_reset_password = False
 
         # Variables to store the position of the mouse
         self._is_dragging = False
         self._start_pos = None
-
-        # Enable Scrolling to Content Page and Display Content Page
 
         # Connect buttons to close, minimize, and maximize the program
         self.CloseButton.clicked.connect(self.close_program)
@@ -56,22 +56,61 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # User Account
         self.LoginButton.clicked.connect(self.process_login)
+        self.GoResetPasswordButton.clicked.connect(self.setup_reset_password_email)
         self.BuatAkunButton.clicked.connect(self.process_account_creation)
         self.SimpanDataButton.clicked.connect(self.process_account_data)
+        self.ProcessResetPassword.clicked.connect(self.process_reset_password_email)
+        self.DashboardEditDataButton.clicked.connect(self.logout_data)
+
+        # Set up the First Condition to Boot up the Program
+        self.ContentFrame.setCurrentIndex(0)
+        self.NotificatioFrame.hide()
 
 # Program Panel Functions______________________________________________________________________________________________________________________
-    def mouse_press_event(self, event):
+    def mousePressEvent(self, event):
         if event.button() == QtCore.Qt.LeftButton:
             self._is_dragging = True
             self._start_pos = event.pos()
 
-    def mouse_move_event(self, event):
+    def mouseMoveEvent(self, event):
         if self._is_dragging:
             self.move(self.pos() + event.pos() - self._start_pos)
 
-    def mouse_release_event(self, event):
+    def mouseReleaseEvent(self, event):
         if event.button() == QtCore.Qt.LeftButton:
             self._is_dragging = False
+
+    def contextMenuEvent(self, event):
+
+        menu = QMenu(self)
+        restore_action = menu.addAction("Restore")
+        # restore_action.setIcon(QIcon(":/feather/copy.svg"))
+        move_action = menu.addAction("Move")
+        size_action = menu.addAction("Size")
+        minimize_action = menu.addAction("Minimize")
+        # minimize_action.setIcon(QIcon(":/feather/minus.svg"))
+        maximize_action = menu.addAction("Maximize")
+        # maximize_action.setIcon(QIcon(":/feather/square.svg"))
+        close_action = menu.addAction("Close Program")
+        # close_action.setIcon(QIcon(":/feather/x.svg"))
+        # Icons broke idk why
+
+        action = menu.exec_(self.mapToGlobal(event.pos()))
+                
+        if action == restore_action:
+            self.showNormal()
+        elif action == move_action:
+            self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+            self.show()
+        elif action == size_action:
+            self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+            self.showMaximized()
+        elif action == minimize_action:
+            self.minimize_program()
+        elif action == maximize_action:
+            self.resize_program()
+        elif action == close_action:
+            self.close()
 
     def close_program(self):
         self.close()
@@ -122,6 +161,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.DashboardControlContainer.hide()
             self.ProfilePageLoginContainer.show()
         else:
+            username = self.accounts.read_credentials()
+            self.ProfilePagePhoto.setText("Selamat Datang " + username + "!")
+
             self.ProfilePagePhotoContainer.show()
             self.DashboardControlContainer.show()
             self.ProfilePageLoginContainer.hide()
@@ -145,9 +187,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def close_notification_page(self):
         self.NotificatioFrame.hide()
 
+    
+
 # Account Functions______________________________________________________________________________________________________________________
     # Login Functions
     def login_page(self):
+        self.ResetPasswordContainer.hide()
         self.LoginLabelStatus.hide()
         self.NotificatioFrame.hide()
         self.ContentFrame.setCurrentIndex(1)
@@ -155,7 +200,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def process_login(self):
         parse_username = self.UsernameBox.toPlainText()
         parse_password = self.PasswordBox.toPlainText()
-        # print(parse_username, parse_password) Debugg Purpose
         self.is_login = self.accounts.user_login(parse_username, parse_password)
         
         if self.is_login == True:
@@ -164,10 +208,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             time.sleep(2)
             self.ContentFrame.setCurrentIndex(0)
             self.ProfilePageLoginContainer.hide()
+            self.UsernameBox.clear()
+            self.PasswordBox.clear()
         else:
             self.LoginLabelStatus.show()
             time.sleep(2)
             self.LoginLabelStatus.setText("Username atau Password Salah!")
+            self.PasswordBox.clear()
 
     # Register Functions
     def register_page(self):
@@ -182,37 +229,102 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.KolomData.show()
 
     def process_account_creation(self):
-        parse_username = self.BuatUserBox.toPlainText()
-        parse_password = self.BuatPasswordBox.toPlainText()
-        parse_repassword = self.BuatRePasswordBox.toPlainText()
-        self.is_match = self.accounts.create_account_username_password(parse_username, parse_password, parse_repassword)
+        # I'm Sorry there is no other way to do this
+        if self.is_this_button_clicked == False:
+            parse_username = self.BuatUserBox.toPlainText()
+            parse_password = self.BuatPasswordBox.toPlainText()
+            parse_repassword = self.RePassBox.toPlainText()
+            self.is_match = self.accounts.create_account_username_password(parse_username, parse_password, parse_repassword)
 
-        if self.is_match == True:
-            self.RegisterFlag.show()
-            self.RegisterFlag.setText("Selamat Akun Anda Telah Dibuat!")
-            time.sleep(2)
-            self.register_data_page()
+            if self.is_match == True:
+                self.RegisterFlag.show()
+                self.RegisterFlag.setText("Selamat Akun Anda Telah Dibuat!")
+                time.sleep(2)
+                self.register_data_page()
+                self.is_match = False
+            else:
+                self.RegisterFlag.show()
+                self.RegisterFlag.setText("Password Tidak Cocok!")
+                time.sleep(2)
+                self.RegisterFlag.setText("Silahkan Coba Lagi!")
+        
         else:
-            self.RegisterFlag.show()
-            self.RegisterFlag.setText("Password Tidak Cocok!")
-            time.sleep(2)
-            self.RegisterFlag.setText("Silahkan Coba Lagi!")
+            parse_password = self.BuatPasswordBox.toPlainText()
+            parse_repassword = self.RePassBox.toPlainText()
+            self.is_match = self.accounts.reset_user_password(parse_password, parse_repassword)
+
+            if self.is_match == True:
+                self.RegisterFlag.show()
+                self.RegisterFlag.setText("Password Telah Diubah!")
+                time.sleep(2)
+                self.RegisterFlag.setText("Silahkan Login!")
+                self.ContentFrame.setCurrentIndex(1)
+                self.is_match = False
+            else:
+                self.RegisterFlag.show()
+                self.RegisterFlag.setText("Password Tidak Cocok!")
+                time.sleep(2)
+                self.RegisterFlag.setText("Silahkan Coba Lagi!")
     
     def process_account_data(self):
         parse_nama = self.NamaBox.toPlainText()
         parse_pekerjaan = self.PekerjaanBox.toPlainText()
+        parse_email = self.AlamatEmailBox.toPlainText()
         parse_gaji = self.GajiBox.toPlainText()
         parse_tempat_kerja = self.TempatKerjaBox.toPlainText()
         parse_tanggungan = self.TanggunganBox.toPlainText()
         parse_cicilan = self.CicilanBox.toPlainText()
         parse_tabungan = self.TabunganBox.toPlainText()
-        self.accounts.create_account_data(parse_nama, parse_pekerjaan, parse_gaji, parse_tempat_kerja, parse_tanggungan, parse_cicilan, parse_tabungan)
+        self.accounts.create_account_data(parse_nama, parse_pekerjaan, parse_email, parse_gaji, parse_tempat_kerja, parse_tanggungan, parse_cicilan, parse_tabungan)
         time.sleep(2)
         self.SavedFlag.show()
         self.SavedFlag.setText("Data Anda Telah Tersimpan!")
         time.sleep(2)
         self.is_login = True
         self.ContentFrame.setCurrentIndex(0)
+
+    def logout_data(self):
+        self.is_login = False
+        self.ProfilePageLoginContainer.show()
+        self.ProfilePagePhotoContainer.hide()
+        self.DashboardControlContainer.hide()
+        self.ContentFrame.setCurrentIndex(0)
+
+    # Reset Password
+    def setup_reset_password_email(self):
+        self.ResetFlagStatus.hide()
+        self.UsernameContainer.hide()
+        self.PasswordContainer.hide()
+        self.LoginButtonContainer.hide()
+        self.ResetPasswordContainer.show()
+        self.ResetFlagStatus.hide()
+        self.ContentFrame.setCurrentIndex(1)
+
+    def process_reset_password_email(self):
+        parse_email = self.EmailBox.toPlainText()
+        self.is_on_reset_password = self.accounts.search_user_email(parse_email)
+
+        if self.is_on_reset_password == True:
+            self.process_reset_password()
+            self.is_on_reset_password = False
+        else:
+            self.ResetFlagStatus.show()
+            self.ResetFlagStatus.setText("Email Tidak Terdaftar!")
+            time.sleep(2)
+            self.ResetFlagStatus.setText("Silahkan Coba Lagi!")
+    
+    def process_reset_password(self):
+        self.KolomData.hide()
+        self.RegisterFlag.hide()
+        self.BuatUserContainer.hide()
+        self.BuatPasswordBox.clear()
+        self.RePassBox.clear()
+        self.BuatAkunLabel.setText("Reset Password")
+        self.BuatAkunButton.setText("Tetapkan Password Baru")
+        self.ContentFrame.setCurrentIndex(2)
+
+# Kredit Functions______________________________________________________________________________________________________________________
+    
 
 if __name__ == "__main__":
     import sys
